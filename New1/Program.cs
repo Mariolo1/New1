76ce09f584ce295557f01cf3eda4db429f58bdcf
Dell <grabowski.mariusz7@gmail.com>
2023-01-17 23:12:54 +0100

    Zadanie nr 4

--- New1/Program.cs
+++ New1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Mario";
+char sex = 'M';
+var age = (17);
+if (age < 18 && sex == 'M') ;
+Console.WriteLine(name + " Niepełnoletni Mężczyzna" );
+
+string name1 = "Ewa";
+var age1 = 33;
+char sex1 = 'K';
+if (sex1 == 'K' && age1 == 33) ;
+Console.WriteLine(name1 + ", lat " + age1);
+
+var age2 = 28 ;
+char sex2 = 'K';
+if (sex2 =='K' &&  age2 < 30) ;
+Console.WriteLine("Kobieta poniżej 30 lat");
