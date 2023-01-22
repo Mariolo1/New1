0daa9bdb4370efc15584495b7f73455e70fb4e7a
Dell <grabowski.mariusz7@gmail.com>
2023-01-22 17:17:14 +0100

    Lekcja nr 5

--- New1/Program.cs
+++ New1/Program.cs
@@ -1,13 +1,33 @@
-﻿using System;
+﻿using System.Globalization;
 
-List<string> daysofweek = new List<string>();
-daysofweek.Add("Poniedziałek");
-daysofweek.Add("Wtorek");
-daysofweek.Add("Środa");
-daysofweek.Add("Czwartek");
-daysofweek.Add("Piątek");
-daysofweek.Add("Sobota");
-daysofweek.Add("Niedziela");
+string input = Console.ReadLine();
+var input1 = int.Parse(input);
 
-foreach (var Day in daysofweek)
-    Console.WriteLine(Day);
+string numberinstring = input1.ToString();
+char[] letters = numberinstring.ToArray();
+
+List<char> numbers = new List<char>();
+numbers.Add('0');
+numbers.Add('1');
+numbers.Add('2');
+numbers.Add('3');
+numbers.Add('4');
+numbers.Add('5');
+numbers.Add('6');
+numbers.Add('7');
+numbers.Add('8');
+numbers.Add('9');
+foreach (var singular in numbers)
+{
+    int count = 0;
+    foreach(var l in letters)
+
+    {
+        if (singular == l) count++;
+    }
+    Console.WriteLine(singular + ">" + count);
+
+
+}
+
+
