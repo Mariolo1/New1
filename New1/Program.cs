6adc3dd0fa4b7ea0c08fdfeb525574f215484880
Dell <grabowski.mariusz7@gmail.com>
2023-01-22 20:30:16 +0100

    Zadanie nr5

--- New1/Program.cs
+++ New1/Program.cs
@@ -3,8 +3,7 @@
 var input = Console.ReadLine();
 if(!int.TryParse(input, out _)) { return; }
 
-string numberinstring = input.ToString();
-char[] letters = numberinstring.ToArray();
+char[] letters = input.ToArray();
 
 List<char> numbers = new List<char>();
 numbers.Add('0');
@@ -17,14 +16,14 @@ numbers.Add('6');
 numbers.Add('7');
 numbers.Add('8');
 numbers.Add('9');
-foreach (var singular in numbers)
+    foreach (var singular in numbers)
 {
     int count = 0;
     foreach(var l in letters)
-
     {
-        if (singular == l) count++;
+        if (singular == l) { count++; };
     }
+
     Console.WriteLine(singular + ">" + count);
 }
 
