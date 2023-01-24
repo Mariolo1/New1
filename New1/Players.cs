6985e01a52851a549b975d43dcce921161cd6d86
Dell <grabowski.mariusz7@gmail.com>
2023-01-24 19:16:43 +0100

    Zadanie nr 6

---
+++ New1/Players.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New1
+{
+    public class Players
+    {
+        private List<int> score = new List<int>();
+
+
+        public Players(string name, string surname, string age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+                    }
+
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public string Age { get; private set; }
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+        public void AddScore(int number)
+        {
+            this.score.Add(number);
+        }
+    }
+}
+    
+
--- New1/Program.cs
+++ New1/Program.cs
@@ -1,12 +1,44 @@
 ﻿using New1;
 using System.Runtime.CompilerServices;
 
-User user1 = new User("Mariolo" , "1234");
-User user2 = new User("Monika" ,"1234");
-User user3 = new User("Ola" ,"1234" );
-User user4 = new User("Franek" , "1234" );
+Players player1 = new Players("Marek" ,"Kowalski ","34");
+Players player2 = new Players("Marek", "Nowak ", "39");
+Players player3 = new Players("Mirek", "Frankowski ", "44");
 
-user1.AddScore(5);
-user1.AddScore(2);
-var result = user1.Result;
-Console.WriteLine(result);
+
+player1.AddScore(5);
+player1.AddScore(9);
+player1.AddScore(7);
+player1.AddScore(8);
+player1.AddScore(6);
+// 35
+
+player2.AddScore(5);
+player2.AddScore(6);
+player2.AddScore(4);
+player2.AddScore(8);
+player2.AddScore(5);
+// 28
+
+player3.AddScore(5);
+player3.AddScore(1);
+player3.AddScore(2);
+player3.AddScore(4);
+player3.AddScore(3);
+// 15
+
+
+if (player1.Result >= player2.Result && player1.Result >= player3.Result)
+{
+    Console.WriteLine($"Najwyższy wynik osiągnął ; { player1.Name}  { player1.Surname} wiek: {player1.Age}  jego wynik to:  {player1.Result}.");
+}
+
+else if (player2.Result >= player1.Result && player2.Result >= player3.Result)
+{
+    Console.WriteLine($"Najwyższy wynik osiągnął ; {player2.Name}  {player2.Surname} wiek: {player2.Age}  jego wynik to:  {player2.Result}.");
+}
+
+else if (player3.Result >= player1.Result && player3.Result >= player2.Result)
+{
+    Console.WriteLine($"Najwyższy wynik osiągnął ; {player3.Name}  {player3.Surname} wiek: {player3.Age}  jego wynik to:  {player3.Result}.");
+}
